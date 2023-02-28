3e5206c0ca1660342e1d2217761ca826c05b2cc9
evan <neff.evank@gmail.com>
2023-02-27 18:31:25 -0700

    Initial Commit

---
+++ Hamburger.py
@@ -0,0 +1,16 @@
+# Group 10 Hamburger Project
+# Evan Neff, Dawson Newell, Jake Millet, Tanner Child, Cameron Pennock, Paxton Davis
+
+class Order():
+    def __init__():
+        pass
+
+
+class Person():
+    def __init__(self) -> None:
+        pass
+
+
+class Customer(Person):
+    def __init__(self) -> None:
+        pass
