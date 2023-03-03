1a51f239d7e05d9855c4e40074075915b33ae85f
evan <neff.evank@gmail.com>
2023-03-03 08:28:28 -0700

    Hamburger Progress

---
+++ Hamburger2.py
@@ -0,0 +1,39 @@
+# Group 10 Hamburger Project
+# Evan Neff, Dawson Newell, Jake Millet, Tanner Child, Cameron Pennock, Paxton Davis
+
+import random
+
+
+class Order():
+    def __init__(self):
+        self.burger_count = self.randomBurgers()
+
+    def randomBurgers(self):
+        self.number_of_burgers = random.randint(1, 20)
+        return self.number_of_burgers
+
+
+class Person():
+    def __init__(self):
+        self.customer_name = self.randomName()
+
+    def randomName(self):
+        self.asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander",
+                            "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
+        randnumber = random.randint(0, 8)
+        return self.asCustomers[randnumber]
+
+
+class Customer(Person):
+    def __init__(self):
+        super().__init__()
+        self.order = Order()
+
+
+queue = Customer()
+
+testOrder = Order()
+testName = Person()
+
+print(testOrder.burger_count)
+print(testName.customer_name)
