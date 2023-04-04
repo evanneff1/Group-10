7708c90fcfc95ce62459b315e095df49ae5e794f
tchild1 <childtanner@gmail.com>
2023-04-04 11:42:07 -0600

    added static folder

--- Missing_Persons/missing_persons/home/urls.py
+++ Missing_Persons/missing_persons/home/urls.py
@@ -3,6 +3,6 @@ from .views import indexPageView, aboutView, contView
 
 urlpatterns = [
     path("", indexPageView, name="Index"),
-    path("about", aboutView, name="About"),
-    path("contributions", contView, name="Contributions"),
+    path("about/", aboutView, name="About"),
+    path("contributions/", contView, name="Contributions"),
 ]
